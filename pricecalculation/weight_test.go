fce0b85bd930f959022843311fed4389c682256d
bas <22396258+bazsup@users.noreply.github.com>
2026-05-13 03:10:39 +0700

    test: weight

--- pricecalculation/weight_test.go
+++ pricecalculation/weight_test.go
@@ -4,30 +4,32 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"implementing-ddd-in-go/pricecalculation"
 )
 
 func TestNewWeight_Zero(t *testing.T) {
-	_, err := pricecalculation.NewWeightFromKG(0)
-	require.NoError(t, err)
+	weight := pricecalculation.NewWeightFromKG(0)
+
+	assert.Equal(t, uint(0), weight.Amount())
 }
 
 func TestNewWeight_Positive(t *testing.T) {
-	weight, err := pricecalculation.NewWeightFromKG(50)
-	require.NoError(t, err)
-	assert.Equal(t, 50, weight.Amount())
+	weight := pricecalculation.NewWeightFromKG(50)
+
+	assert.Equal(t, uint(50), weight.Amount())
 }
 
 func TestWeight_Equals_SameWeight(t *testing.T) {
-	a, _ := pricecalculation.NewWeightFromKG(50)
-	b, _ := pricecalculation.NewWeightFromKG(50)
+	a := pricecalculation.NewWeightFromKG(50)
+	b := pricecalculation.NewWeightFromKG(50)
+
 	assert.True(t, a.Equals(b))
 }
 
 func TestWeight_Equals_DifferentWeight(t *testing.T) {
-	a, _ := pricecalculation.NewWeightFromKG(50)
-	b, _ := pricecalculation.NewWeightFromKG(100)
+	a := pricecalculation.NewWeightFromKG(50)
+	b := pricecalculation.NewWeightFromKG(100)
+	
 	assert.False(t, a.Equals(b))
 }
